464b4611b4751ff5e146d22bae60876321dd42f8
Abdou Mandara <abdoumandara@gmail.com>
2026-09-10 15:46:13 +0100

    Update : Retour sur le chargement de la police a travers congig du plugin dans app.json

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,19 +1,27 @@
 import { Tabs } from 'expo-router';
-
+import { Home, AlarmClock} from 'lucide-react-native';
 export default function TabsLayout() {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Accueil',
+          title: 'GestMoney | Accueil',
+          tabBarLabel: 'Accueil',
+          tabBarIcon : ({color, size}) => (
+            <Home color={color} size={size} />
+          )
         }}
       />
 
       <Tabs.Screen
         name="history"
         options={{
-          title: 'Historique',
+          title: 'GestMoney | Historique',
+          tabBarLabel: 'Historique',
+          tabBarIcon : ({color, size})=>{
+            <AlarmClock color={color} size={size} />
+          }
         }}
       />
 
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack screenOptions={ { headerShown: false } }/>;
+  const [fontsLoaded, fontError] = useFonts({
+    "Roboto Slab": require("../../assets/fonts/RobotoSlab-VariableFont_wght.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
