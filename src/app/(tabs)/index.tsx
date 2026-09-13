1223a3cea2f0795bd1ffbd0b1e8aac6d2384a0db
Abdou Mandara <abdoumandara@gmail.com>
2026-09-13 01:48:09 +0100

    Retaf sur la view d'affichage du montant total, depense et gains

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,37 +1,27 @@
 import "../../../global.css";
 import { ScrollView,View, Text } from "react-native";
-import { Wallet } from "lucide-react-native";
+import CardTotal from "../components/CardTotal";
+import { MoveUpRight,  MoveDownLeft,  } from "lucide-react-native";
 export default function Index() {
   return (
-    <View className="flex items-center h-full py-8 gap-4">
-      <View className="flex flex-column items-center mb-4 w-[95%] gap-2 bg-blue-500 rounded-2xl px-4 py-6">
-        <View className="w-full flex items-center gap-2">
-          <Text className="text-2xl font-semibold text-white" style={{ fontFamily: 'Roboto Slab' }}>L'argent dans tes poches :</Text>
-          <Text className="text-2xl font-semibold text-white text-center" style={{ fontFamily: 'Roboto Slab' }}><Text className="text-6xl font-bold text-white" style={{ fontFamily: 'Roboto Slab' }}>0</Text> FCFA</Text>
+    <View className="flex items-center h-full gap-4 bg-blue-500">
+      <View className="flex flex-column mt-8 items-center mb-4 w-[95%] gap-8 rounded-2xl p-4">
+        <View className="w-full flex items-center gap-4  border-white">
+          <Text className="text-xl font-semibold text-white" style={{ fontFamily: 'Roboto Slab' }}>L' argent dans tes poches </Text>
+          <Text className="text-4xl font-semibold text-white flex text-center" style={{ fontFamily: 'Roboto Slab' }}><Text className="text-6xl font-bold text-white" style={{ fontFamily: 'Roboto Slab' }}>0</Text> FCFA</Text>
         </View>
 
+        <View className="h-[25%] w-full justify-between px-1 flex flex-row overflow-hidden">
+          <CardTotal title="Gains" icone={<MoveUpRight color="rgba(14, 250, 45, 0.84)" size={20}/>} price={50} signe="+" />
+          <CardTotal title="Depenses" icone={<MoveDownLeft color="rgb(207, 13, 13)" size={20}/>} price={40} signe="-"/>
+        </View>
         {/* <View className="flex-1">
           <Wallet color="white" size={50}  />
         </View> */}
       </View>
 
-      <View className="flex-1 w-full bg-purple-500 py-4 gap-2">
-        <Text className="text-2xl" style={{ fontFamily: 'Roboto Slab' }}>Regardes ton travail, <Text className="font-bold">Abdou</Text> :(</Text>
-        <View className="relative z-0 h-[25%] w-full bg-white flex flex-row rounded-2xl overflow-hidden">
-          <View className="flex-1 z-0 h-full bg-red-400/30 flex py-8 items-center">
-            <Text className="text-white font-semibold text-2xl" style={{ fontFamily: 'Roboto Slab' }}>Depenses</Text>
-            <Text className="text-white font-bold text-6xl" style={{ fontFamily: 'Roboto Slab' }}>0</Text>
-            <Text className="text-white font-italic text-xl" style={{ fontFamily: 'Roboto Slab' }}>FCFA</Text>
-          </View>
-          <View className="flex-1 z-0 h-full bg-green-400/30 flex py-8 items-center">
-            <Text className="text-white font-semibold text-2xl" style={{ fontFamily: 'Roboto Slab' }}>Gains</Text>
-            <Text className="text-white font-bold text-6xl" style={{ fontFamily: 'Roboto Slab' }}>0</Text>
-            <Text className="text-white font-italic text-xl" style={{ fontFamily: 'Roboto Slab' }}>FCFA</Text>
-          </View>
-          <View className="h-[55px] w-[55px] flex justify-center items-center rounded-full absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white ">
-            <Text className="text-2xl font-bold text-black" style={{ fontFamily: 'Roboto Slab' }}>VS</Text>
-          </View>
-        </View>
+      <View className="flex-1 w-full h-full rounded-l-3xl bg-white rounded-r-3xl pt-6 gap-4">
+        <Text className="text-2xl text-center " style={{ fontFamily: 'Roboto Slab' }}>Regardes ton travail, <Text className="font-bold">Abdou</Text> !</Text>
       </View>
     </View>
   );
---
+++ src/app/components/CardTotal.tsx
@@ -0,0 +1,19 @@
+import { ReactElement } from "react";
+import {View, Text } from "react-native";
+interface CardProps{
+    title : String,
+    icone : ReactElement,
+    price : number,
+    signe : String,
+}
+export default function CardTotal({title, icone, price, signe}: CardProps){
+    return(
+          <View className="w-[45%] flex justify-center gap-2 items-center rounded-2xl">
+            <View className="flex flex-row gap-2 items-center justify-center">
+                {icone}
+                <Text className="text-white text-xl" style={{ fontFamily: 'Roboto Slab' }}>{title}</Text>
+            </View>
+            <Text className="text-white flex text-center justify-center text-2xl" >{signe} <Text className="text-white font-semibold text-3xl" style={{ fontFamily: 'Roboto Slab' }}>{price} F</Text></Text>
+        </View>
+    )
+}
